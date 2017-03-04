a37e086f50dd3c59f06cee9a1a7f5e239e485d67
Ivo Georgiev <ivo@linvo.org>
2017-03-04 20:13:34 +0200

    satanic II

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'vicious' });
+  res.render('index', { title: 'satanic' });
 });
 
 module.exports = router;
