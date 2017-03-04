e341f4dfaef0f601ba272ea3e3c1fddd28f12084
Ivo Georgiev <ivo@linvo.org>
2017-03-04 21:30:46 +0200

    random from many gifs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var gifs = require("../gifs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'satanic' });
+  res.render('index', { 
+	title: 'satanic',
+	pic: gifs[Math.floor(gifs.length*Math.random())]
+  });
 });
 
 module.exports = router;
