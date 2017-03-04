c8dc2d9139b49465c7aeb3f87e406d1f9a767ce5
Ivo Georgiev <ivo@linvo.org>
2017-03-04 21:35:08 +0200

    rotate gifs

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ var router = express.Router();
 var gifs = require("../gifs");
 
 /* GET home page. */
+var i = 0;
+
 router.get('/', function(req, res, next) {
+  i++;
+  if (i > gifs.length-1) i=0; 
   res.render('index', { 
 	title: 'satanic',
-	pic: gifs[Math.floor(gifs.length*Math.random())]
+	pic: gifs[i]
   });
 });
 
